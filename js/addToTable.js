0fda05584fffc3ecf1a0408502018b6e545bcc6b
Sergey <shornikov1407@gmail.com>
2023-09-12 23:08:14 +0300

    remake localStorage

--- js/addToTable.js
+++ js/addToTable.js
@@ -1,34 +1,10 @@
-//
-// function addToTable() {
-//     let output = '';
-//     let table = document.getElementById("table_out");
-//     let cookie_params = Cookies.get("input_data");
-//     if (cookie_params !== undefined && cookie_params !== ""){
-//         let array = cookie_params.split("/");
-//         console.log(array);
-//         for (let i = 0; i < array.length - 1; i++){
-//             console.log(array[i]);
-//             let values = array[i].split(";");
-//             output += "<tr>";
-//             for (let j = 0; j < values.length; j++){
-//                 console.log(values[j]);
-//                 output += "<td>";
-//                 output += values[j];
-//                 output += "</td>";
-//             }
-//             output += "</tr>";
-//         }
-//         table.innerHTML = output;
-//     }
-// }
-// addToTable();
 
 function addToTable() {
     let table = document.getElementById("table_out");
-    let cookie_params = Cookies.get("input_data");
+    let localStorageData = localStorage.getItem("input_data");
 
-    if (cookie_params !== undefined && cookie_params !== ""){
-        let array = cookie_params.split("/");
+    if (localStorageData !== null && localStorageData !== "") {
+        let array = localStorageData.split("/");
         console.log(array);
 
         // Очистить таблицу перед добавлением новых данных
@@ -36,11 +12,11 @@ function addToTable() {
             table.removeChild(table.firstChild);
         }
 
-        for (let i = 0; i < array.length - 1; i++){
+        for (let i = 0; i < array.length - 1; i++) {
             console.log(array[i]);
             let values = array[i].split(";");
             let row = document.createElement("tr"); // Создаем новую строку
-            for (let j = 0; j < values.length; j++){
+            for (let j = 0; j < values.length; j++) {
                 console.log(values[j]);
                 let cell = document.createElement("td"); // Создаем новую ячейку
                 cell.textContent = values[j]; // Устанавливаем текст ячейки
--- js/sendData.js
+++ js/sendData.js
@@ -1,24 +1,30 @@
-let cookies_data = '';
-function sendData(){
+
+let localStorageData = '';
+
+function sendData() {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "server.php?" + "x=" + userInputX + "&y=" + userInputY + "&r=" + userInputR);
     xhr.send();
-    xhr.onreadystatechange = function (){
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
-            if (Cookies.get("input_data") !== undefined){
-                cookies_data = Cookies.get("input_data");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            if (localStorage.getItem("input_data") !== null) {
+                localStorageData = localStorage.getItem("input_data");
             }
-            cookies_data += phpResponse(xhr.responseText);
-            Cookies.set("input_data", cookies_data);
-            console.log(Cookies.get("input_data"));
-            addToTable()
+            localStorageData += phpResponse(xhr.responseText);
+            localStorage.setItem("input_data", localStorageData);
+            // console.log(localStorage.getItem("input_data"));
+            addToTable();
         }
-    }
+    };
 }
-function phpResponse(answer){
-    if (answer.split("/")[0].split(";")[0] ===""){
-        alert("отправленные данные не корректны");
+function phpResponse(answer) {
+    if (answer.split("/")[0].split(";")[0] === "") {
+        alert("Отправленные данные не корректны");
+        return null;
+    }else {
+        drawPoint();
+        return answer;
     }
-    drawPoint();
-    return answer;
+
+
 }
