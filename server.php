3595315c2011d113bca18bf987dccb4bb1e6579d
RaiseBan <shornikov1407@gmail.com>
2023-09-13 17:59:55 +0300

    fixed validation

--- server.php
+++ server.php
@@ -1,27 +1,36 @@
 <?php
 $start_time = microtime();
 $flag = true;
-if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['x']) && isset($_GET['y']) && isset($_GET['r'])){
     $response = '';
     $res = '';
     $x = $_GET['x'];
     $y = $_GET['y'];
     $r = $_GET['r'];
-    if ((!is_int($x)) && ($x < -4 || $x > 4)){
+    if ((!is_numeric($x)) || ($x < -4 || $x > 4)){
         $flag = false;
+        http_response_code(422);
+        echo "not passed validation....3";
+        exit();
     }
 
-    if ((!is_numeric($y)) && ($y < -3 || $y > 3)){
+    if ((!is_numeric($y)) || ($y <= -3 || $y >= 3)){
         $flag = false;
+        http_response_code(422);
+        echo "not passed validation....1";
+        exit();
     }
-    if ((!is_int($y)) && ($r < 1 || $r > 5)){
+    if ((!is_numeric($r)) || ($r < 1 || $r > 5)){
         $flag = false;
+        http_response_code(422);
+        echo "not passed validation....2";
+        exit();
     }
 
     if ($flag){
         if ((($x * $x + $y * $y) <= $r * $r / 4 && $x <= 0 && $y >= 0) ||
             (-2 * $y - $x <= $r  && $x <= 0 && $y <= 0) ||
-            ($x >= 0 && $y <= 0 && $x <= (-1) * $r / 2 && $y >= (-1) * $r)) {
+            ($x >= 0 && $y <= 0 && $x <= $r / 2 && $y >= (-1) * $r)) {
             $res = "In";
         } else {
             $res = "Out";
@@ -37,6 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
         $response .= ';';
         $response .= $res;
         $response .= ';';
+        date_default_timezone_set("Europe/Moscow");
         $response .= date("Y-m-d H:i:s");
         $response .= ';';
         $response .= $start_time;
