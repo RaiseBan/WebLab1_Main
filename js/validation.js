487c73f537e8336b8e2e9d71f8fcfdbee505a6d6
Sergey <shornikov1407@gmail.com>
2023-09-27 00:45:17 +0300

    make beautiful alerts and fix validation

--- js/validation.js
+++ js/validation.js
@@ -27,10 +27,15 @@ function checkY (){
         flag = false
         Toastify({
             text: "ну не, не подходит чиселко",
-            duration: 2000,
-            style:{
-                background: "grey"
-            }
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to right, #6e8580, #0c2b2a)",
+            },
+            onClick: function(){} // Callback after click
         }).showToast();
     }else{
         if (userInputY.value > -3 && userInputY.value <= 3){
